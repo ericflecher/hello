bb4a9683686f41afaee0c92cb82a44ccbcfb24bb
Eric Flecher <ericflecher@Eric-Flechers-MacBook-Air.local>
2011-05-14 19:38:35 -0400

    hellowworld

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helloworld::Application.config.secret_token = '5770dc67f7ddbea121200dde15c13f2f89e8d3fd0182be05c9e98f6c6a4b644914093120c98ddc2ff127b7e4163ceae023bd3d9ca6ce3d577b832184de5f3f2d'
